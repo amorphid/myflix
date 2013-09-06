c23b08bbab7509b0fe102618b27d67bbc114ae44
Michael Pope <amorphid@captaincoder.io>
2013-09-05 21:41:16 -0700

    Rewrite model tests using shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,8 @@
 require "spec_helper"
 
 describe Category do
-  let(:category) { Category.new(title: "Banana") }
-
   it { should have_many(:videos) }
   it { should have_many(:video_categories) }
 
-  it "should save w/ title" do
-    expect(category.save).to be_true
-  end
-
-  it "should not save w/o title" do
-    category.title = ""
-    expect(category).to have(1).errors_on(:title)
-  end
+  it { should validate_presence_of(:title) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,39 +1,11 @@
 require "spec_helper"
 
 describe Video do
-  let(:video) do
-    Video.new(
-      title: "A title",
-      description: "A description",
-      small_cover_url: "/foo/bar-small.jpg",
-      large_cover_url: "/foo/bar-large.jpg"
-    )
-  end
-
   it { should have_many(:categories) }
   it { should have_many(:video_categories) }
 
-  it "should save w/ title, description, small_cover_url, & large_cover_url" do
-    expect(video).to be_valid
-  end
-
-  it "should not save w/o title" do
-    video.title = ""
-    expect(video).to have(1).errors_on(:title)
-  end
-
-  it "should not save w/o description" do
-    video.description = ""
-    expect(video).to have(1).errors_on(:description)
-  end
-
-  it "should not save w/o small_cover_url" do
-    video.small_cover_url = ""
-    expect(video).to have(1).errors_on(:small_cover_url)
-  end
-
-  it "should not save w/o large_cover_url" do
-    video.large_cover_url = ""
-    expect(video).to have(1).errors_on(:large_cover_url)
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:small_cover_url) }
+  it { should validate_presence_of(:large_cover_url) }
 end
