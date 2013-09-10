06f9280b04a912719722675f5a1a1e6d2758d41a
Michael Pope <amorphid@captaincoder.io>
2013-09-09 20:57:11 -0700

    Refactor User model test to only create User as validating emails, the only test that needs it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,12 @@
   require "spec_helper"
 
-      describe User do
-        before(:each) do
-          User.create(full_name: "Bob", email: "bob@bob.com", password: "bob", password_confirmation: "bob")
-        end
+  describe User do
+    it do
+      FactoryGirl.create(:user)
+      should validate_uniqueness_of(:email)
+    end
 
-        it { should validate_presence_of(:email) }
-        it { should validate_presence_of(:full_name) }
-        it { should validate_presence_of(:password_digest) }
-
-        it { should validate_uniqueness_of(:email) }
-      end
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:full_name) }
+    it { should validate_presence_of(:password_digest) }
+  end
