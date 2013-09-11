77122ec450903ca96098bd6e026662c2028e7fde
Michael Pope <amorphid@captaincoder.io>
2013-09-10 20:21:52 -0700

    Add tests for User controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,62 @@ require "spec_helper"
 
 describe UsersController do
   context "GET new" do
+    it "sets @user" do
+      get :new
+      expect(assigns(:user)).to be_a(User)
+    end
   end
+
+  context "POST create" do
+    it "sets @User" do
+      post :create, user: {}
+      expect(assigns(:user)).to be_instance_of(User)
+    end
+
+    it "creates the user with valid input" do
+      user = Fabricate.build(:user)
+      post :create, user: { full_name:            user.full_name,
+                            email:                 user.email,
+                            password:              user.password,
+                            password_confirmation: user.password_confirmation }
+
+      expect(User.last.email).to eq(user.email)
+    end
+
+    it "redirects to sign_in_path with valid input" do
+      user = Fabricate.build(:user)
+      post :create, user: { full_name:             user.full_name,
+                            email:                 user.email,
+                            password:              user.password,
+                            password_confirmation: user.password_confirmation }
+
+      expect(response).to redirect_to sign_in_path
+    end
+
+    it "does not create user with duplicate email address" do
+      existing_user = Fabricate(:user)
+      new_user      = Fabricate.build(:user)
+      post :create, user: { full_name:             new_user.full_name,
+                            email:                 existing_user.email,
+                            password:              new_user.password,
+                            password_confirmation: new_user.password_confirmation }
+
+      expect(response).to render_template :new
+    end
+
+    it "does not create user with invalit input (such as missing email)" do
+      new_user      = Fabricate.build(:user)
+      post :create, user: { full_name:             new_user.full_name,
+                            password:              new_user.password,
+                            password_confirmation: new_user.password_confirmation }
+
+      expect(response).to render_template :new
+    end
+  end
+end
+
+
+
 
 
   # context "GET index" do
@@ -48,4 +103,4 @@ describe UsersController do
   #     expect(response).to redirect_to root_path
   #   end
   # end
-end
+
