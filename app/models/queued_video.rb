945e6efeaab29b3d1bdf9833baa3ac198d85390d
Michael Pope <amorphid@captaincoder.io>
2013-09-19 00:19:46 -0700

    Add test to ensure QueuedVideo priority is a positive Integer before saving

--- app/models/queued_video.rb
+++ app/models/queued_video.rb
@@ -2,9 +2,19 @@ class QueuedVideo < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
 
+  before_save :priority_is_positive_integer?
+
   validates :priority, presence: true
   validates :user_id,  presence: true
   validates :video_id, presence: true
 
   validates_uniqueness_of :user_id, scope: :video_id
+
+  def priority_is_positive_integer?
+    if priority.is_a? Integer
+      priority > 0 ? true : false
+    else
+      false
+    end
+  end
 end
--- spec/models/queued_video_spec.rb
+++ spec/models/queued_video_spec.rb
@@ -8,5 +8,33 @@ describe QueuedVideo do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:video_id) }
 
-  it { should validate_uniqueness_of(:user_id).scoped_to(:video_id) }
+  it "should validate uniqueness of user_id and video_id" do
+    QueuedVideo.create(priority: 1, user_id: 1, video_id: 1)
+    queued_video = QueuedVideo.new(priority: 1, user_id: 1, video_id: 1)
+    expect(queued_video.save).to eq(false)
+  end
+
+  describe "#is_positive_integer?" do
+    let(:queued_video) { Fabricate(:queued_video) }
+
+    it "returns true if positive integer" do
+      queued_video.priority = 3
+      expect(queued_video.priority_is_positive_integer?).to eq(true)
+    end
+
+    it "returns false if not an integer" do
+      queued_video.priority = "banana"
+      expect(queued_video.priority_is_positive_integer?).to eq(false)
+    end
+
+    it "returns false if 0" do
+      queued_video.priority = 0
+      expect(queued_video.priority_is_positive_integer?).to eq(false)
+    end
+
+    it "returns false if negative number" do
+      queued_video.priority = -7
+      expect(queued_video.priority_is_positive_integer?).to eq(false)
+    end
+  end
 end
