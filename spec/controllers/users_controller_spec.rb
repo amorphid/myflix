1e7a6594b1fa76dda9e66aad8129a92bf5adf4b3
Michael Pope <amorphid@captaincoder.io>
2013-09-26 15:32:50 -0700

    Refactor SessionsController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ describe UsersController do
   describe "GET new" do
     it "sets @user" do
       get :new
-      expect(assigns(:user)).to be_a(User)
+      expect(assigns(:user)).to be_instance_of(User)
     end
   end
 
