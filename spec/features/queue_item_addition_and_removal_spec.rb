bc215b4c56f72eab6500eb4666843854c1ebd9f9
Michael Pope <amorphid@captaincoder.io>
2013-09-30 17:28:47 -0700

    Create a sign_in macro for feature tests

--- spec/features/queue_item_addition_and_removal_spec.rb
+++ spec/features/queue_item_addition_and_removal_spec.rb
@@ -3,15 +3,11 @@ require "spec_helper"
 feature "queue item addition and removal" do
   given(:href)         { "/videos/" + video.id.to_s + "/queued_videos/" + queued_video.id.to_s }
   given(:video)        { Fabricate(:video) }
-  given(:user )        { Fabricate(:user)  }
+  given(:user )        { User.last  }
   given(:queued_video) { Fabricate(:queued_video, user_id: user.id, video_id: video.id) }
+  given(:removal_note) { video.title + " successfully removed from your queue" }
 
-  background do
-    visit sign_in_path
-    fill_in      "Email",    with: user.email
-    fill_in      "Password", with: user.password
-    click_button "Sign in"
-  end
+  background { sign_in }
 
   scenario "adds items upon clicking '+ My Queue' link" do
     visit video_path(video)
@@ -23,13 +19,13 @@ feature "queue item addition and removal" do
     queued_video
     visit video_path(video)
     click_link("- My Queue")
-    expect(page).to have_content(video.title + " successfully removed from your queue")
+    expect(page).to have_content(removal_note)
   end
 
   scenario "removes items upon clicking 'X' button" do
     queued_video
     visit my_queue_path
     find(:xpath, "//a[@href='#{href}']").click
-    expect(page).to have_content(video.title + " successfully removed from your queue")
+    expect(page).to have_content(removal_note)
   end
 end
