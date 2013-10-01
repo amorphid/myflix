3b9494e5e7cfab304d59a377293c786e3080d542
Michael Pope <amorphid@captaincoder.io>
2013-10-01 12:37:25 -0700

    Add feature test for updating priority of videos in the queue

--- spec/features/updating_order_of_items_in_queue_spec.rb
+++ spec/features/updating_order_of_items_in_queue_spec.rb
@@ -1,8 +1,37 @@
 require "spec_helper"
 
 feature "Updating order of items in queue" do
-  scenario "using positive integer"
-  scenario "using negative integer"
-  scenario "using positive float"
-  scenario "using negative float"
+  before do
+    sign_in
+    Fabricate.times(2, :queued_video)
+    visit my_queue_path
+  end
+
+  scenario "using positive integer" do
+    fill_in :video_1_priority, with: "3"
+    click_button("Update Queue")
+    expect(find("#video_2_priority").value).to eq("1")
+    expect(find("#video_1_priority").value).to eq("2")
+  end
+
+  scenario "using negative integer" do
+    fill_in :video_2_priority, with: "-1"
+    click_button("Update Queue")
+    expect(find("#video_2_priority").value).to eq("1")
+    expect(find("#video_1_priority").value).to eq("2")
+  end
+
+  scenario "using positive float" do
+    fill_in :video_1_priority, with: "2.2"
+    click_button("Update Queue")
+    expect(find("#video_2_priority").value).to eq("1")
+    expect(find("#video_1_priority").value).to eq("2")
+  end
+
+  scenario "using negative float" do
+    fill_in :video_2_priority, with: "-1.1"
+    click_button("Update Queue")
+    expect(find("#video_2_priority").value).to eq("1")
+    expect(find("#video_1_priority").value).to eq("2")
+  end
 end
