3f8b5f4829cbbbf3d19cc612afd49143c26a38ff
Michael Pope <amorphid@captaincoder.io>
2013-10-01 22:11:03 -0700

    Remove requirement for a user to have a small_pic_url

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,6 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:password_digest) }
-  it { should validate_presence_of(:small_pic_url) }
 
   it do
     Fabricate(:user)
