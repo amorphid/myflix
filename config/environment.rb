3000d5df5fbc5a1c470e67e0ef5c8c7f4b751e11
Michael Pope <amorphid@captaincoder.io>
2013-10-10 12:27:31 -0700

    Update environment.rb to use newer Ruby hash syntax

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Myflix::Application.initialize!
 # sending email
 ActionMailer::Base.default_url_options = { host: ENV["domain"] }
 ActionMailer::Base.smtp_settings = {
-  :user_name =>     ENV["email_username"],
-  :password =>      ENV["email_password"],
-  :domain =>        ENV["domain"],
-  :address =>       "smtp.sendgrid.net",
-  :port =>           587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  user_name:            ENV["email_username"],
+  password:             ENV["email_password"],
+  domain:               ENV["domain"],
+  address:              "smtp.sendgrid.net",
+  port:                 587,
+  authentication:       :plain,
+  enable_starttls_auto: true
 }
