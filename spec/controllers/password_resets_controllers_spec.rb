73318fab8cd10c44eadd07959b6e42d75b8ac6a6
Michael Pope <amorphid@captaincoder.io>
2013-10-12 11:01:17 -0700

    Move tests for #request_password_reset to password_resets_controller_spec.rb

--- spec/controllers/password_resets_controllers_spec.rb
+++ spec/controllers/password_resets_controllers_spec.rb
@@ -1,4 +1,29 @@
 require "spec_helper"
 
 describe PasswordResetsController do
+  describe "POST request_reset_password" do
+    context "with valid email" do
+      let(:user) { Fabricate(:user) }
+
+      it "redirects to confirm password reset page" do
+        post :request_password_reset, email: user.email
+        expect(response).to redirect_to confirm_password_reset_path
+      end
+
+      it "sends email for resetting password" do
+        post :request_password_reset, email: user.email
+        expect(ActionMailer::Base.deliveries).not_to be_empty
+      end
+    end
+
+    it "redirects to sign in page w/ invalid email" do
+      post :request_password_reset, email: "invalid@email.address"
+      expect(response).to redirect_to forgot_password_path
+    end
+
+    it "redirects to sign in page w/ blank email" do
+      post :request_password_reset, email: ""
+      expect(response).to redirect_to forgot_password_path
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,32 +8,6 @@ describe UsersController do
     end
   end
 
-  describe "POST request_reset_password" do
-    context "with valid email" do
-      let(:user) { Fabricate(:user) }
-
-      it "redirects to confirm password reset page" do
-        post :request_password_reset, email: user.email
-        expect(response).to redirect_to confirm_password_reset_path
-      end
-
-      it "sends email for resetting password" do
-        post :request_password_reset, email: user.email
-        expect(ActionMailer::Base.deliveries).not_to be_empty
-      end
-    end
-
-    it "redirects to sign in page w/ invalid email" do
-      post :request_password_reset, email: "invalid@email.address"
-      expect(response).to redirect_to forgot_password_path
-    end
-
-    it "redirects to sign in page w/ blank email" do
-      post :request_password_reset, email: ""
-      expect(response).to redirect_to forgot_password_path
-    end
-  end
-
   describe "POST password_reset" do
     let(:user)  { Fabricate(:user) }
     let(:token) { user.password_reset_token }
